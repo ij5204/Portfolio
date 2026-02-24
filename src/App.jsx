52436d5b08907df442889dddec2c956e377b19bd
Ishitaa Jain <ishitaaj2004@gmail.com>
2026-02-24 10:25:46 -0600

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { Analytics } from "@vercel/analytics/next"
 import { motion, AnimatePresence, useMotionValue, useSpring, useTransform } from "framer-motion";
 import {
   ArrowRight,
