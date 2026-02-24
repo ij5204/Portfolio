0375b0a6f38cb562281b7bfeb42eb11e6d9d74f0
Ishitaa Jain <ishitaaj2004@gmail.com>
2026-02-24 10:37:10 -0600

    Speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { motion, AnimatePresence, useMotionValue, useSpring, useTransform } from "framer-motion";
 import {
   ArrowRight,
